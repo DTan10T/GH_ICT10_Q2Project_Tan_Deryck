c8690d322edbf7c318c2426622c4dbac4572dd72
DTan10T <tan.dg@obmontessori.edu.ph>
2026-01-19 23:11:05 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+from pyscript import display, document
+About the school: our school has been in operation since 2015 and it has a lot of course for students to pick such as business management
+    and teaching etc.
+
+If you want to inquire in our school feel free to contact us # pyright: ignore[reportUndefinedVariable]
